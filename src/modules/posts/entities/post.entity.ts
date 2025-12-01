e06c812225b113b422b5c90c461c6a770f9c2fc4
vietnhan1342002 <cudum13402nhan@gmail.com>
2025-12-01 18:44:40 +0700

    feat(post): add Post entity and Post module

---
+++ src/modules/posts/entities/post.entity.ts
@@ -0,0 +1,26 @@
+import { User } from 'src/modules/users/entities/user.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column({ type: 'text' })
+  content: string;
+
+  @ManyToOne<User>(() => User, (user) => user.posts, { onDelete: 'SET NULL' })
+  author: User;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
