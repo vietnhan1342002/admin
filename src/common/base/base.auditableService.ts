5e9573ec3b5d903165ef709f898ebff3ef794f5e
vietnhan1342002 <cudum13402nhan@gmail.com>
2025-12-14 15:48:54 +0700

    refactor(base): add BaseMapper to base layer

--- src/common/base/base.auditableService.ts
+++ src/common/base/base.auditableService.ts
@@ -2,34 +2,41 @@ import { DeepPartial, ObjectLiteral } from 'typeorm';
 import { BaseService } from './base.service';
 import { BaseRepository } from './base.repository';
 import { UserContextService } from './user.context';
+import { BaseMapper } from './base.mapper';
+import { NotFoundException } from '@nestjs/common';
 
 export abstract class AuditableBaseService<
   T extends ObjectLiteral,
   CreateDTO extends DeepPartial<T>,
   UpdateDTO extends DeepPartial<T>,
-> extends BaseService<T, CreateDTO, UpdateDTO> {
+  ResponseDTO,
+> extends BaseService<T, CreateDTO, UpdateDTO, ResponseDTO> {
   constructor(
     repository: BaseRepository<T>,
     protected readonly userContext: UserContextService,
+    mapper: BaseMapper<T, ResponseDTO>,
   ) {
-    super(repository);
+    super(repository, mapper);
   }
 
-  async create(data: CreateDTO): Promise<T> {
+  async create(data: CreateDTO): Promise<ResponseDTO> {
     await this.beforeCreate(data);
     console.log('Creating by user:', this.userContext.userId);
 
-    return this.repository.create({
+    const result = await this.repository.create({
       ...data,
       author: this.userContext.userId,
     });
+    return this.mapper.toResponse(result);
   }
 
-  async update(id: string, data: UpdateDTO): Promise<T | null> {
+  async update(id: string, data: UpdateDTO): Promise<ResponseDTO | null> {
     await this.beforeUpdate(id, data);
-    return this.repository.update(id, {
+    const result = await this.repository.update(id, {
       ...data,
       author: this.userContext.userId,
     });
+    if (!result) throw new NotFoundException('Item not found');
+    return this.mapper.toResponse(result);
   }
 }
