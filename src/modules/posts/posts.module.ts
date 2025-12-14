e775e3000dc009b16d03b34f1acbd592e9c80614
vietnhan1342002 <cudum13402nhan@gmail.com>
2025-12-14 15:46:59 +0700

    feat(post): add PostMapper

--- src/modules/posts/posts.module.ts
+++ src/modules/posts/posts.module.ts
@@ -5,11 +5,12 @@ import { Post } from './entities/post.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostRepository } from './repositories/post.repository';
 import { UserContextService } from 'src/common/base/user.context';
+import { PostMapper } from './mapper/post.mapper';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Post])],
   controllers: [PostsController],
-  providers: [PostsService, PostRepository, UserContextService],
+  providers: [PostsService, PostRepository, UserContextService, PostMapper],
   exports: [PostsService],
 })
 export class PostsModule {}
