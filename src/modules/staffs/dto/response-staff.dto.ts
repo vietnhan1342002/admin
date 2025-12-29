5633922546157a6f947b4e65fd4e9f83dc086173
vietnhan1342002 <cudum13402nhan@gmail.com>
2025-12-29 11:05:42 +0700

    reafactor(staff): redesign staff entity

--- src/modules/staffs/dto/response-staff.dto.ts
+++ src/modules/staffs/dto/response-staff.dto.ts
@@ -4,11 +4,7 @@ import { StaffStatus } from '../enum/staff.enum';
 export class StaffResponseDto {
   id: string;
   user: UserResponseDto | null;
-  firstName: string;
-  lastName: string;
-  avatarUrl?: string;
   position: string;
-  phone: string;
   status: StaffStatus;
   dateAdded: Date;
   createdAt: Date;
--- src/modules/staffs/entities/staff.entity.ts
+++ src/modules/staffs/entities/staff.entity.ts
@@ -5,10 +5,7 @@ import { StaffStatus } from '../enum/staff.enum';
 
 @Entity()
 @Index(['userId', 'deletedAt'], { unique: true })
-@Index(['firstName', 'deletedAt'])
-@Index(['lastName', 'deletedAt'])
 @Index(['position', 'deletedAt'])
-@Index(['phone', 'deletedAt'], { unique: true })
 @Index(['status', 'deletedAt'])
 export class Staff extends BaseEntity {
   @Column({ name: 'user_id' })
@@ -18,20 +15,8 @@ export class Staff extends BaseEntity {
   @JoinColumn({ name: 'user_id' })
   user: User;
 
-  @Column({ name: 'first_name', length: 100 })
-  firstName: string;
-
-  @Column({ name: 'last_name', length: 100 })
-  lastName: string;
-
-  @Column({ name: 'avatar_url', type: 'text', nullable: true })
-  avatarUrl?: string;
-
   @Column({ length: 100 })
   position: string;
-
-  @Column({ length: 20 })
-  phone: string;
 
   @Column({
     type: 'enum',
