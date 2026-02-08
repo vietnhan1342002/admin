635140deb68c863ba472d6727c40dddf1e33c883
vietnhan1342002 <cudum13402nhan@gmail.com>
2026-02-08 17:17:02 +0700

    refactor(data-source): add MYSQL option for railwaty

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,19 +1,26 @@
 import 'dotenv/config';
 import { DataSource } from 'typeorm';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default new DataSource({
   type: 'mysql',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT) || 3306,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database:
-    process.env.NODE_ENV === 'production'
-      ? process.env.DB_NAME
-      : process.env.DB_NAME_DEV,
 
-  entities: ['src/modules/**/entities/*.entity.ts'],
-  migrations: ['src/database/migrations/*.ts'],
+  host: process.env.DB_HOST || process.env.MYSQL_HOST,
+  port: Number(process.env.DB_PORT || process.env.MYSQL_PORT || 3306),
+  username: process.env.DB_USER || process.env.MYSQL_USER,
+  password: process.env.DB_PASSWORD || process.env.MYSQL_PASSWORD,
+  database: isProd
+    ? process.env.DB_NAME || process.env.MYSQL_DATABASE
+    : process.env.DB_NAME_DEV,
+
+  entities: isProd
+    ? ['dist/modules/**/entities/*.entity.js']
+    : ['src/modules/**/entities/*.entity.ts'],
+
+  migrations: isProd
+    ? ['dist/database/migrations/*.js']
+    : ['src/database/migrations/*.ts'],
 
   synchronize: false,
 });
