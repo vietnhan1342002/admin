10dbd6b54aa33be554e641cc3694985bb815a1ff
vietnhan1342002 <cudum13402nhan@gmail.com>
2026-02-10 17:53:59 +0700

    refactor(data-source): reorder MySQL_ and DB_ variables in .env

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,13 +6,14 @@ const isProd = process.env.NODE_ENV === 'production';
 export default new DataSource({
   type: 'mysql',
 
-  host: process.env.DB_HOST || process.env.MYSQL_HOST,
-  port: Number(process.env.DB_PORT || process.env.MYSQL_PORT || 3306),
-  username: process.env.DB_USER || process.env.MYSQL_USER,
-  password: process.env.DB_PASSWORD || process.env.MYSQL_PASSWORD,
-  database: isProd
-    ? process.env.DB_NAME || process.env.MYSQL_DATABASE
-    : process.env.DB_NAME_DEV,
+  host: process.env.MYSQL_HOST || process.env.DB_HOST,
+  port: Number(process.env.MYSQL_PORT || process.env.DB_PORT || 3306),
+  username: process.env.MYSQL_USER || process.env.DB_USER,
+  password: process.env.MYSQL_PASSWORD || process.env.DB_PASSWORD,
+  database:
+    process.env.NODE_ENV === 'production'
+      ? process.env.MYSQL_DATABASE || process.env.DB_NAME
+      : process.env.DB_NAME_DEV,
 
   entities: isProd
     ? ['dist/modules/**/entities/*.entity.js']
