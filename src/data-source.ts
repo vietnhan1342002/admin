026bac87a674b4efa2af9819beb7a5a5d73235d1
vietnhan1342002 <cudum13402nhan@gmail.com>
2026-02-10 17:57:59 +0700

    refactor(data-source): delete DB_ in migration

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,14 +6,14 @@ const isProd = process.env.NODE_ENV === 'production';
 export default new DataSource({
   type: 'mysql',
 
-  host: process.env.MYSQL_HOST || process.env.DB_HOST,
-  port: Number(process.env.MYSQL_PORT || process.env.DB_PORT || 3306),
-  username: process.env.MYSQL_USER || process.env.DB_USER,
-  password: process.env.MYSQL_PASSWORD || process.env.DB_PASSWORD,
+  host: process.env.MYSQL_HOST,
+  port: Number(process.env.MYSQL_PORT || 3306),
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
   database:
     process.env.NODE_ENV === 'production'
-      ? process.env.MYSQL_DATABASE || process.env.DB_NAME
-      : process.env.DB_NAME_DEV,
+      ? process.env.MYSQL_DATABASE
+      : 'admin',
 
   entities: isProd
     ? ['dist/modules/**/entities/*.entity.js']
